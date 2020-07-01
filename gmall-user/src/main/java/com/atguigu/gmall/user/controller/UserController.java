34b1a4f39e6aa4da630843a971982ebf55bf1b4f
tianzhen <tianzhen@qq.com>
2020-07-01 17:40:27 +0800

    查询所有

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.atguigu.gmall.user.controller;
+
+import com.atguigu.gmall.user.bean.UmsMember;
+import com.atguigu.gmall.user.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("getAll")
+    public List<UmsMember> getAll() {
+        List<UmsMember> umsMembers = userService.getAll();
+        return umsMembers;
+    }
+}
---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/services/UserService.java
@@ -0,0 +1,9 @@
+package com.atguigu.gmall.user.services;
+
+import com.atguigu.gmall.user.bean.UmsMember;
+
+import java.util.List;
+
+public interface UserService {
+    List<UmsMember> getAll();
+}
